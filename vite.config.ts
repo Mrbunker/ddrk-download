a49c83258cbd32599a02d54e1935dc5a5b476e83
mrbunker <Mrbunker@qq.com>
2022-11-05 23:18:20 +0800

    打个包包

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: { charset: "utf8" },
   plugins: [
     monkey({
       entry: "src/main.ts",
